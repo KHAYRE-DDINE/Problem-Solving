030b1ad2427ab50c4a720f799a5f416231c15ac9
Khayreddine Aharrar <khirdin.mimi@gmail.com>
2023-09-03 00:53:52 +0100

    Another Challenge

---
+++ Date Sanity Methods.js
@@ -0,0 +1,11 @@
+function diSemVowel(str) {
+
+    str = str.replace(/a|e|i|u|o/ig, "")
+
+
+    return console.log(str);
+}
+
+diSemVowel("This website is for losers LOL!")// "Ths wbst s fr lsrs LL!"
+diSemVowel("No offense but,\nYour writing is among the worst I've ever read")// "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
+diSemVowel("What are you, a communist?")//"Wht r y,  cmmnst?"
---
+++ NATO Phonetic Alphabet.js
@@ -0,0 +1,47 @@
+// Complete the function nato that takes a word in parameter and returns a string that spells the word using the NATO phonetic alphabet.
+// There should be a space between each word in the returned string, and the first letter of each word should be capitalized.
+// For those of you that don't want your fingers to bleed, this kata already has a dictionary typed out for you.
+
+
+let LETTERS = {
+    a: 'Alfa',
+    b: 'Bravo',
+    c: 'Charlie',
+    d: 'Delta',
+    e: 'Echo',
+    f: 'Foxtrot',
+    g: 'Golf',
+    h: 'Hotel',
+    i: 'India',
+    j: 'Juliet',
+    k: 'Kilo',
+    l: 'Lima',
+    m: 'Mike',
+    n: 'November',
+    o: 'Oscar',
+    p: 'Papa',
+    q: 'Quebec',
+    r: 'Romeo',
+    s: 'Sierra',
+    t: 'Tango',
+    u: 'Uniform',
+    v: 'Victor',
+    w: 'Whiskey',
+    x: 'Xray',
+    y: 'Yankee',
+    z: 'Zulu'
+}
+
+// the object LETTERS is preloaded
+// LETTERS['B'] === 'Bravo', etc
+function nato(word) {
+    let natoPhonetic = word.split("").map((el) => {
+        return LETTERS[el.toLowerCase()]
+    }).join(" ")
+    return console.log(natoPhonetic);
+}
+
+nato("hi")// "Hotel India";
+nato("abc")// "Alpha Bravo Charlie";
+nato("babble")// "Bravo Alpha Bravo Bravo Lima Echo";
+nato("Banana")// "Bravo Alpha November Alpha November Alpha";
